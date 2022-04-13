4974128d5d1bcfe43c9cccd2450f5e9e5746faef
Michael Owuor <michael.owuor@gmail.com>
2022-04-13 08:31:04 +0300

    feat: firebase setup.

---
+++ src/firebase.jsx
@@ -0,0 +1,19 @@
+/** import functions needed from the sdk */
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+/** web firebase configuration */
+const firebaseConfig = {
+	apiKey: 'AIzaSyB78pD7AQx-aZH9gtbvAvXOBcYP0FuPndA',
+	authDomain: 'slp-firebase-authentication.firebaseapp.com',
+	projectId: 'slp-firebase-authentication',
+	storageBucket: 'slp-firebase-authentication.appspot.com',
+	messagingSenderId: '762000607193',
+	appId: '1:762000607193:web:732cf7c9bb60f072ae3bc8',
+};
+
+/** initialize firebase */
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export default app;
